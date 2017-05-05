433e009c3db745f8e548030930d15d02c8341368
marmalade <summit@tutanota.de>
2017-05-05 15:36:16 -0700

    disabled imenu-list-minor-mode

--- config.el
+++ config.el
@@ -7,4 +7,4 @@
 
 (add-hook 'fountain-mode-hook 'fountain-face-mode-courier)
 (add-hook 'fountain-mode-hook 'olivetti-mode)
-(add-hook 'fountain-mode-hook 'imenu-list-minor-mode)
+;;(add-hook 'fountain-mode-hook 'imenu-list-minor-mode)
